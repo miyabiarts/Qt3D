c65d779fe342269002c639c8a868ebe1298f1f23
miyabiarts <miyabiarts@gmail.com>
2011-07-08 18:06:00 +0900

    形状を追加

--- Demo3/Viewport.h
+++ Demo3/Viewport.h
@@ -3,7 +3,11 @@
 #include <Qt3D/QGLView.h>
 #include <Qt3D/QGLBuilder.h>
 #include <Qt3D/QGLCube.h>
+#include <Qt3D/QGLSphere.h>
+#include <Qt3D/QGLCylinder.h>
+#include <Qt3D/QGLTeapot.h>
 #include <QTimer>
+#include <QKeyEvent>
 
 class Viewport : public QGLView
 {
@@ -16,11 +20,31 @@ public:
 
 	void initializeGL(QGLPainter * painter)
     {
+		type = 0;
+
+		//
 		QGLBuilder builder;
         builder << QGL::Faceted;
         builder << QGLCube();
-
         cube = builder.finalizedSceneNode();
+
+		//
+		QGLBuilder builder2;
+        builder2 << QGL::Faceted;
+        builder2 << QGLSphere();
+        sphere = builder2.finalizedSceneNode();
+
+		//
+		QGLBuilder builder3;
+		builder3 << QGL::Faceted;
+        builder3 << QGLCylinder();
+        cylinder = builder3.finalizedSceneNode();
+		
+		//
+		QGLBuilder builder4;
+		builder4 << QGL::Faceted;
+        builder4 << QGLTeapot();
+        teapot = builder4.finalizedSceneNode();
 
 		angle = 0.0f;
 
@@ -37,12 +61,52 @@ public:
 		QMatrix4x4 m;
 		m.rotate(angle, 0.0f, 1.0f, 0.0f );
 		painter->modelViewMatrix() *= m;
-		cube->draw(painter);
+		if( type == 0 )
+		{
+			cube->draw(painter);
+		}
+		else if( type == 1 )
+		{
+			sphere->draw(painter);
+		}
+		else if( type == 2 )
+		{
+			cylinder->draw(painter);
+		}
+		else
+		{
+			teapot->draw(painter);
+		}
 
 		angle += 1.0f;
 	}
 
 private:
 	QGLSceneNode *cube;
+	QGLSceneNode *sphere;
+	QGLSceneNode *cylinder;
+	QGLSceneNode *teapot;
+
+	int type;
+
+	void keyPressEvent( QKeyEvent *key )
+    {
+		if( key->key() == Qt::Key_A)
+		{
+			type = 0;
+		}		
+		else if( key->key() == Qt::Key_S)
+		{
+			type = 1;
+		}
+		else if( key->key() == Qt::Key_D)
+		{
+			type = 2;
+		}
+		else if( key->key() == Qt::Key_F)
+		{
+			type = 3;
+		}
+	 }
 	float angle;
 };
--- Demo4/Viewport.h
+++ Demo4/Viewport.h
@@ -3,7 +3,11 @@
 #include <Qt3D/QGLView.h>
 #include <Qt3D/QGLBuilder.h>
 #include <Qt3D/QGLCube.h>
+#include <Qt3D/QGLSphere.h>
+#include <Qt3D/QGLCylinder.h>
+#include <Qt3D/QGLTeapot.h>
 #include <QTimer>
+#include <QKeyEvent>
 
 class Viewport : public QGLView
 {
@@ -16,13 +20,35 @@ public:
 
 	void initializeGL(QGLPainter * painter)
     {
+		type = 0;
+
+		//
 		QGLBuilder builder;
         builder << QGL::Faceted;
         builder << QGLCube();
-
         cube = builder.finalizedSceneNode();
 
+		//
+		QGLBuilder builder2;
+        builder2 << QGL::Faceted;
+        builder2 << QGLSphere();
+        sphere = builder2.finalizedSceneNode();
+
+		//
+		QGLBuilder builder3;
+		builder3 << QGL::Faceted;
+        builder3 << QGLCylinder();
+        cylinder = builder3.finalizedSceneNode();
+		
+		//
+		QGLBuilder builder4;
+		builder4 << QGL::Faceted;
+        builder4 << QGLTeapot();
+        teapot = builder4.finalizedSceneNode();
+
 		angle = 0.0f;
+
+		color = QColor(170, 202, 0);
 
 		// ’èŠú“I‚É‰æ–Ê‚ðXV
         QTimer *timer = new QTimer( this );
@@ -39,15 +65,65 @@ public:
 		painter->modelViewMatrix() *= m;
 
         painter->setStandardEffect(QGL::LitMaterial);
-        painter->setFaceColor(QGL::AllFaces, QColor(170, 202, 0));
-//        painter->setFaceColor(QGL::AllFaces, QColor(255, 0, 0));
+        painter->setFaceColor(QGL::AllFaces, color);
 	
-		cube->draw(painter);
+		if( type == 0 )
+		{
+			cube->draw(painter);
+		}
+		else if( type == 1 )
+		{
+			sphere->draw(painter);
+		}
+		else if( type == 2 )
+		{
+			cylinder->draw(painter);
+		}
+		else
+		{
+			teapot->draw(painter);
+		}
 
 		angle += 1.0f;
 	}
 
 private:
 	QGLSceneNode *cube;
+	QGLSceneNode *sphere;
+	QGLSceneNode *cylinder;
+	QGLSceneNode *teapot;
+
 	float angle;
+	int type;
+
+	QColor color;
+
+	void keyPressEvent( QKeyEvent *key )
+    {
+		if( key->key() == Qt::Key_A)
+		{
+			type = 0;
+		}		
+		else if( key->key() == Qt::Key_S)
+		{
+			type = 1;
+		}
+		else if( key->key() == Qt::Key_D)
+		{
+			type = 2;
+		}
+		else if( key->key() == Qt::Key_F)
+		{
+			type = 3;
+		}
+
+		if( key->key() == Qt::Key_Z)
+		{
+			color = QColor(170, 202, 0);
+		}
+		else if( key->key() == Qt::Key_X)
+		{
+			color = QColor(255, 0, 0);
+		}
+	 }
 };
