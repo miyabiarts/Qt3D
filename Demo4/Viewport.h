163e1986d16d45839956040643b5243f6ceccfba
miyabiarts <miyabiarts@gmail.com>
2011-07-08 11:59:47 +0900

    デモその4を追加

---
+++ Demo4/Viewport.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <Qt3D/QGLView.h>
+#include <Qt3D/QGLBuilder.h>
+#include <Qt3D/QGLCube.h>
+#include <QTimer>
+
+class Viewport : public QGLView
+{
+Q_OBJECT
+public:
+	Viewport() :
+	  QGLView(0)
+	{
+	}
+
+	void initializeGL(QGLPainter * painter)
+    {
+		QGLBuilder builder;
+        builder << QGL::Faceted;
+        builder << QGLCube();
+
+        cube = builder.finalizedSceneNode();
+
+		angle = 0.0f;
+
+		// ’èŠú“I‚É‰æ–Ê‚ðXV
+        QTimer *timer = new QTimer( this );
+        connect( timer, SIGNAL( timeout() ), this, SLOT( updateGL() ) );
+        timer->setInterval( 16 );
+        timer->setSingleShot( false );
+        timer->start();
+	}
+
+	void paintGL(QGLPainter *painter)
+	{
+		QMatrix4x4 m;
+		m.rotate(angle, 1.0f, 1.0f, 0.0f );
+		painter->modelViewMatrix() *= m;
+
+        painter->setStandardEffect(QGL::LitMaterial);
+        painter->setFaceColor(QGL::AllFaces, QColor(170, 202, 0));
+		cube->draw(painter);
+
+		angle += 1.0f;
+	}
+
+private:
+	QGLSceneNode *cube;
+	float angle;
+};
---
+++ Demo4/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include "Viewport.h"
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+
+	Viewport view;
+	view.show();
+
+	return a.exec();
+}
